5047c288e99dc1dc9c74069368f4dc50a8619a74
Jeandeson O. Merelis <jean.merelis@gmail.com>
2020-01-31 17:03:02 -0300

    Adicionado Lombok para criar os builders e transformar os DTOs em imutáveis.

--- src/main/java/dev/rinaldo/dto/FrutaDTO.java
+++ src/main/java/dev/rinaldo/dto/FrutaDTO.java
@@ -1,6 +1,12 @@
 package dev.rinaldo.dto;
 
 import dev.rinaldo.domain.Fruta;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import lombok.Value;
 
 /**
  * Exemplo de DTO.
@@ -24,52 +30,15 @@ import dev.rinaldo.domain.Fruta;
  * @author rinaldodev
  *
  */
+@Value
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@EqualsAndHashCode
+@ToString
 public class FrutaDTO {
 
-    public Long id;
-    public String nome;
-    public Integer votos;
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
-        result = prime * result + ((nome == null) ? 0 : nome.hashCode());
-        result = prime * result + ((votos == null) ? 0 : votos.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        FrutaDTO other = (FrutaDTO) obj;
-        if (id == null) {
-            if (other.id != null)
-                return false;
-        } else if (!id.equals(other.id))
-            return false;
-        if (nome == null) {
-            if (other.nome != null)
-                return false;
-        } else if (!nome.equals(other.nome))
-            return false;
-        if (votos == null) {
-            if (other.votos != null)
-                return false;
-        } else if (!votos.equals(other.votos))
-            return false;
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "FrutaDTO [id=" + id + ", nome=" + nome + ", votos=" + votos + "]";
-    }
-
+    private final Long id;
+    private final String nome;
+    private Integer votos;
+  
 }
